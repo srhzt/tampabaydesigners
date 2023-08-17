81d1dacd8d45ce8346c520372944e9295b7807cf
srhzt <65615455+srhzt@users.noreply.github.com>
2023-08-17 16:51:46 -0400

    Update index.js

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,12 +2,12 @@ import Head from 'next/head'
 import { Normalize } from 'styled-normalize'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga-4'
 
 export default function Layout({ children, pageTitle, description, ogImage, ...props }) {
   
   if (typeof window !== "undefined") {
-    ReactGA.initialize('UA-157497184-1')
+    ReactGA.initialize(process.env.GOOGLE_ANALYTICS);
     ReactGA.pageview(window.location.pathname + window.location.search)
   }
 
